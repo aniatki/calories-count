8e3ac6eea78951bc88b8b7b49eff134355657677
Hasan Atki <djamblers@gmail.com>
2022-03-05 02:13:42 +0000

    Define function for searching entries

--- run.py
+++ run.py
@@ -11,8 +11,20 @@ CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('nuitrition_sheet')
-
 food_entries = SHEET.worksheet('ABBREV')
+names_col = food_entries.col_values(1)
+
+def search_for_entry():
+    """
+    Lets the user input text in order to search for a certain food
+    """
+    search_str = input("Type the food you would like to search for.\n")
+    for i in range(len(names_col)):
+        casefolded_names = names_col[i].casefold()
+        food_list = [x for x in names_col]
+        if search_str.casefold() in casefolded_names:
+            print("found\n")
+
 
 class FoodEntry:
     """
@@ -29,9 +41,9 @@ class FoodEntry:
     def description(self):
         return f"{self.name} contains {self.energy}kCal, {self.protein}g of Protein, {self.lipid}g of Saturated Fats, {self.carbs}g of Carbohydrates and {self.fiber}g of Fiber."
 
-food_names = food_entries.col_values(1)
 
-i = 0
-while i < len(food_names):
-    food_names[i].lower()
-    i += 1
+# search_for_entry()
+
+
+a = 6
+print(food_entries.row_values(a))
